f137952dc63084564790be9ddeb7a7dd98d2bd7e
kkangminlee <lkm@paytalab.com>
2022-07-06 14:21:30 +0900

    [#1] buildSrc로 버전관리

---
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    const val packageName = "org.passorder.boss"
+    const val compileSdk = 32
+    const val minSdk = 28
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
