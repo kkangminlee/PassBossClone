ae894d00aa52921265ec88b09e46ada12176b3ee
kkangminlee <lkm@paytalab.com>
2022-07-07 15:34:52 +0900

    [#7] 주문 리스트

---
+++ app/src/main/java/org/passorder/boss/presentation/main/order/OrderFragment.kt
@@ -0,0 +1,14 @@
+package org.passorder.boss.presentation.main.order
+
+import android.os.Bundle
+import android.view.View
+import org.passorder.boss.R
+import org.passorder.boss.databinding.FragmentOrderBinding
+import org.passorder.ui.base.BindingFragment
+
+class OrderFragment: BindingFragment<FragmentOrderBinding>(R.layout.fragment_order) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
