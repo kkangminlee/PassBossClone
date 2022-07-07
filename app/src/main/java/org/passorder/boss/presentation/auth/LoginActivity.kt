a8ebf83156cbe9b30887a4a439afc660518be935
kkangminlee <lkm@paytalab.com>
2022-07-07 15:34:27 +0900

    [#7] 주문내역 ui

---
+++ app/src/main/java/org/passorder/boss/presentation/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.passorder.boss.presentation.auth
+
+import android.os.Bundle
+import org.passorder.boss.R
+import org.passorder.boss.databinding.ActivityLoginBinding
+import org.passorder.ui.base.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
