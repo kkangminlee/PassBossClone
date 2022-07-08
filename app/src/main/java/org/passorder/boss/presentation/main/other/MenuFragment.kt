cf0a63fae548f4c06c73922215bb27567221ddab
kkangminlee <lkm@paytalab.com>
2022-07-08 14:28:29 +0900

    [#9] 더미 Fragment

---
+++ app/src/main/java/org/passorder/boss/presentation/main/other/MenuFragment.kt
@@ -0,0 +1,13 @@
+package org.passorder.boss.presentation.main.other
+
+import android.os.Bundle
+import android.view.View
+import org.passorder.boss.R
+import org.passorder.boss.databinding.FragmentEmptyBinding
+import org.passorder.ui.base.BindingFragment
+
+class MenuFragment: BindingFragment<FragmentEmptyBinding>(R.layout.fragment_empty) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
