71097a8e39cd2df73722763e5539dd6affa8362f
kkangminlee <lkm@paytalab.com>
2022-07-12 11:11:59 +0900

    [#11] 유저정보 조회 Repository

--- data/src/main/java/org/passorder/data/service/AuthService.kt
+++ data/src/main/java/org/passorder/data/service/AuthService.kt
@@ -1,17 +1,21 @@
 package org.passorder.data.service
 
 import org.passorder.data.model.response.ResponseLogin
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import org.passorder.data.model.response.ResponseUser
+import retrofit2.http.*
 
 interface AuthService {
     @FormUrlEncoded
-    @POST("/v2/users/authentication")
+    @POST("users/authentication")
     suspend fun postLogin(
         @Field("login_id") loginId : String,
         @Field("password") password : String,
         @Field("fcm_token") fcmToken : String?,
         @Field("app_agent") appAgent : Int?
     ): ResponseLogin
+
+    @GET("users/{uuid}")
+    suspend fun getUser(
+        @Path("uuid") uuid: String
+    ): ResponseUser
 }
--- domain/src/main/java/org/passorder/domain/repository/AuthRepository.kt
+++ domain/src/main/java/org/passorder/domain/repository/AuthRepository.kt
@@ -2,7 +2,9 @@ package org.passorder.domain.repository
 
 import org.passorder.domain.entity.LoginToken
 import org.passorder.domain.entity.PostLogin
+import org.passorder.domain.entity.User
 
 interface AuthRepository {
     suspend fun postLogin(request: PostLogin): LoginToken
+    suspend fun getUser(uuid: String): User
 }
