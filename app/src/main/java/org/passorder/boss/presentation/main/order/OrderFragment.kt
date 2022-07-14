9a25bc42080c3999cb9c205eb82dc44df757bbe8
kkangminlee <lkm@paytalab.com>
2022-07-14 16:30:06 +0900

    [#13] 현재 주문 내역 구현

--- app/src/main/java/org/passorder/boss/presentation/main/order/OrderFragment.kt
+++ app/src/main/java/org/passorder/boss/presentation/main/order/OrderFragment.kt
@@ -2,13 +2,57 @@ package org.passorder.boss.presentation.main.order
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.passorder.boss.R
 import org.passorder.boss.databinding.FragmentOrderBinding
+import org.passorder.domain.entity.SetOrder
 import org.passorder.ui.base.BindingFragment
 
-class OrderFragment: BindingFragment<FragmentOrderBinding>(R.layout.fragment_order) {
+@AndroidEntryPoint
+class OrderFragment : BindingFragment<FragmentOrderBinding>(R.layout.fragment_order) {
+    private var adapter: OrderListAdapter? = null
+    private val viewModel by viewModels<OrderViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        observe()
+    }
+
+    private fun initView() {
+        adapter = OrderListAdapter{
+            viewModel.putOrderStatus(it.identifier)
+        }
+        binding.rvOrderList.adapter = adapter
+    }
+
+    private fun observe() {
+        viewModel.currentOrder.flowWithLifecycle(lifecycle)
+            .onEach {
+                binding.clEmpty.isVisible = it.isEmpty()
+                binding.rvOrderList.isVisible = it.isNotEmpty()
+                adapter?.submitList(it)
+            }.launchIn(lifecycleScope)
+
+        viewModel.orderStatus.flowWithLifecycle(lifecycle)
+            .onEach {
+                //Todo Status 응답값 처리
+            }.launchIn(lifecycleScope)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.orderList(SetOrder(1, 9999, 0, null, null))
+    }
+
+    override fun onDestroyView() {
+        adapter = null
+        super.onDestroyView()
     }
 }
---
+++ app/src/main/java/org/passorder/boss/presentation/main/order/OrderViewModel.kt
@@ -0,0 +1,45 @@
+package org.passorder.boss.presentation.main.order
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
+import org.passorder.domain.entity.Order
+import org.passorder.domain.entity.SetOrder
+import org.passorder.domain.entity.Status
+import org.passorder.domain.repository.OrderRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderViewModel @Inject constructor(
+    private val repository: OrderRepository
+) : ViewModel() {
+    private val _currentOrder = MutableStateFlow<List<Order>>(listOf())
+    val currentOrder = _currentOrder.asStateFlow()
+
+    private val _orderStatus = MutableStateFlow<Status?>(null)
+    val orderStatus = _orderStatus.asStateFlow().filterNotNull()
+
+    fun orderList(request: SetOrder) {
+        viewModelScope.launch {
+            runCatching {
+                repository.orderList(request)
+            }.onSuccess {
+                _currentOrder.value = it
+            }
+        }
+    }
+
+    fun putOrderStatus(uuid: String) {
+        viewModelScope.launch {
+            runCatching {
+                repository.putOrderStatus(uuid)
+            }.onSuccess {
+                _orderStatus.value = it
+            }
+        }
+    }
+}
