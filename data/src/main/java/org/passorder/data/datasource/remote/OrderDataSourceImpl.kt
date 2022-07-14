a16a81c09d6c72fc7881cb146ce1366169d07b6b
kkangminlee <lkm@paytalab.com>
2022-07-14 16:38:38 +0900

    [#13] 현재 & 과거 주문 목록 DataSource

--- data/src/main/java/org/passorder/data/datasource/remote/OrderDataSourceImpl.kt
+++ data/src/main/java/org/passorder/data/datasource/remote/OrderDataSourceImpl.kt
@@ -1,6 +1,12 @@
 package org.passorder.data.datasource.remote
 
 import org.passorder.data.datasource.OrderDataSource
+import org.passorder.data.model.request.RequestCount
+import org.passorder.data.model.request.RequestOrder
+import org.passorder.data.model.response.ResponseCount
+import org.passorder.data.model.response.ResponseMoney
+import org.passorder.data.model.response.ResponseOrder
+import org.passorder.data.model.response.ResponseStatus
 import org.passorder.data.service.OrderService
 import org.passorder.domain.PassDataStore
 import javax.inject.Inject
@@ -15,5 +21,23 @@ class OrderDataSourceImpl @Inject constructor(
 
     override suspend fun setMinTimeStore(time: Int) {
         service.setMinTimeStore(dataStore.storeUUID, time)
+    }
+
+    override suspend fun putOrderStatus(uuid: String):ResponseStatus {
+        return service.putOrderStatus(uuid)
+    }
+
+    override suspend fun orderList(request: RequestOrder): List<ResponseOrder> {
+        return service.orderList(request.page, request.limit, request.filter, request.start, request.end).map {
+            it.translate()
+        }
+    }
+
+    override suspend fun totalMoney(request: RequestCount): List<ResponseMoney> {
+        return service.getTotalMoney(request.start, request.end, request.includeDump, request.conditionDump)
+    }
+
+    override suspend fun orderCount(request: RequestCount): List<ResponseCount> {
+        return service.getOrderCount(request.start,request.end,request.includeDump,request.conditionDump)
     }
 }
