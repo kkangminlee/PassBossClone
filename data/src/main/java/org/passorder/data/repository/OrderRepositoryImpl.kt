559f00261034af1214b25d8bb1ca4d416180a144
kkangminlee <lkm@paytalab.com>
2022-07-14 16:38:59 +0900

    [#13] 현재 & 과거 주문 목록 Repository

--- data/src/main/java/org/passorder/data/repository/OrderRepositoryImpl.kt
+++ data/src/main/java/org/passorder/data/repository/OrderRepositoryImpl.kt
@@ -1,7 +1,8 @@
 package org.passorder.data.repository
 
 import org.passorder.data.datasource.OrderDataSource
-import org.passorder.domain.PassDataStore
+import org.passorder.data.model.request.toRequest
+import org.passorder.domain.entity.*
 import org.passorder.domain.repository.OrderRepository
 import javax.inject.Inject
 
@@ -14,5 +15,27 @@ class OrderRepositoryImpl @Inject constructor(
 
     override suspend fun setMinTimeStore(time: Int) {
         dataSource.setMinTimeStore(time)
+    }
+
+    override suspend fun putOrderStatus(uuid: String): Status {
+        return dataSource.putOrderStatus(uuid).toStatus()
+    }
+
+    override suspend fun totalMoney(request: SetCount): List<Money> {
+        return dataSource.totalMoney(request.toRequest()).map {
+            it.toMoney()
+        }
+    }
+
+    override suspend fun orderList(request: SetOrder): List<Order> {
+        return dataSource.orderList(request.toRequest()).map {
+            it.toOrder()
+        }
+    }
+
+    override suspend fun orderCount(request: SetCount): List<Count> {
+        return dataSource.orderCount(request.toRequest()).map {
+            it.toCount()
+        }
     }
 }
--- domain/src/main/java/org/passorder/domain/repository/OrderRepository.kt
+++ domain/src/main/java/org/passorder/domain/repository/OrderRepository.kt
@@ -1,6 +1,12 @@
 package org.passorder.domain.repository
 
+import org.passorder.domain.entity.*
+
 interface OrderRepository {
     suspend fun gateStore(isOpen: Boolean)
     suspend fun setMinTimeStore(time: Int)
+    suspend fun putOrderStatus(uuid: String): Status
+    suspend fun orderList(request: SetOrder): List<Order>
+    suspend fun orderCount(request: SetCount): List<Count>
+    suspend fun totalMoney(request: SetCount): List<Money>
 }
