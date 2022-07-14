d1bd4f00b8df9e71b81623480a9c8840ed41ca33
kkangminlee <lkm@paytalab.com>
2022-07-14 16:36:42 +0900

    [#13] Money & Order domain

---
+++ domain/src/main/java/org/passorder/domain/entity/Order.kt
@@ -0,0 +1,44 @@
+package org.passorder.domain.entity
+
+data class SetOrder(
+    val page: Int,
+    val limit: Int,
+    val filter: Int,
+    val start: String?,
+    val end: String?
+)
+
+data class Order(
+    val requests: String,
+    val nickName: String,
+    val price: Int,
+    val phone: String,
+    val createdDate: String,
+    val tableNumber: Int,
+    var methodOfPayment: Int,
+    val number: Number,
+    val status: Int,
+    val identifier: String,
+    val menus: List<Menu>,
+    val usedCoupons: List<Coupon>,
+    val payStatus: String
+)
+
+data class Menu(
+    val count: Int,
+    val name: String,
+    val price: Int,
+    val options: List<Option>
+)
+
+data class Option(
+    val name: String,
+    val price: Int
+)
+
+data class Coupon(
+    val adjustmentsTarget: Int,
+    val benefit: Int,
+    val couponName: String,
+    val ownedBy: Int,
+)
