7e9067a9d0b7a61ff95150e384497684d3ac5d01
kkangminlee <lkm@paytalab.com>
2022-07-14 16:33:26 +0900

    [#13] 과거 주문 내역 동적 추가 구현

--- app/src/main/java/org/passorder/boss/presentation/main/history/HistoryFragment.kt
+++ app/src/main/java/org/passorder/boss/presentation/main/history/HistoryFragment.kt
@@ -6,6 +6,10 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -13,14 +17,18 @@ import org.passorder.boss.R
 import org.passorder.boss.databinding.FragmentHistoryBinding
 import org.passorder.boss.presentation.main.history.dialog.DatePickerDialog
 import org.passorder.boss.presentation.main.order.OrderListAdapter
+import org.passorder.domain.PassDataStore
 import org.passorder.domain.entity.SetCount
 import org.passorder.domain.entity.SetOrder
 import org.passorder.ui.base.BindingFragment
 import java.util.*
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class HistoryFragment: BindingFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
+    @Inject
+    lateinit var dataStore: PassDataStore
     private var adapter: OrderListAdapter? = null
     private val viewModel by viewModels<HistoryViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,6 +44,10 @@ class HistoryFragment: BindingFragment<FragmentHistoryBinding>(R.layout.fragment
 
         }
         binding.rvLastOrder.adapter = adapter
+        val decoration = DividerItemDecoration(requireActivity(), LinearLayoutManager.VERTICAL)
+        binding.rvLastOrder.addItemDecoration(decoration)
+
+
     }
 
     private fun initEvent() {
@@ -44,10 +56,7 @@ class HistoryFragment: BindingFragment<FragmentHistoryBinding>(R.layout.fragment
             dialog.onDateListener { start, end ->
                 setPeriod(start,end)
             }
-            dialog.show(
-                requireFragmentManager(),
-                "dialog"
-            )
+            dialog.show(requireFragmentManager(), "dialog")
         }
     }
 
@@ -58,21 +67,40 @@ class HistoryFragment: BindingFragment<FragmentHistoryBinding>(R.layout.fragment
                 binding.rvLastOrder.isVisible = it.isNotEmpty()
                 adapter?.submitList(it)
             }.launchIn(lifecycleScope)
+
+        viewModel.orderCount.flowWithLifecycle(lifecycle)
+            .onEach {
+                binding.tvOrderCount.text = "${it}개"
+            }.launchIn(lifecycleScope)
+
+        viewModel.money.flowWithLifecycle(lifecycle)
+            .onEach {
+                binding.tvMoney.text = "${it}원"
+            }.launchIn(lifecycleScope)
     }
 
     private fun setPeriod(start: String, end: String) {
+        val includeDump = gsonToJson(listOf(dataStore.storeUUID))
+        val orderCount = gsonToJson(listOf(false,false,false,false,false,false,false,false))
+        val totalMoney = gsonToJson(listOf(false,false,false,false,false,false,false,true))
         binding.tvStartDetail.text = start
         binding.tvEndDetail.text = end
         viewModel.orderList(SetOrder(1,10,1,start,end))
-        viewModel.getOrderCount(SetCount(start,end,null,null))
+        viewModel.getOrderCount(SetCount(start,end,includeDump, orderCount))
+        viewModel.getTotalMoney(SetCount(start,end,includeDump,totalMoney))
     }
 
     override fun onResume() {
         super.onResume()
         val cal = Calendar.getInstance()
-        val startDate = "${cal[Calendar.YEAR]}-${String.format("%02d",cal[Calendar.MONTH]+1)}-${String.format("%02d",cal.getActualMinimum(Calendar.DAY_OF_MONTH))}"
-        val endDate = "${cal[Calendar.YEAR]}-${String.format("%02d",cal[Calendar.MONTH]+1)}-${String.format("%02d",cal.getActualMaximum(Calendar.DAY_OF_MONTH))}"
+        val yearMonth = "${cal[Calendar.YEAR]}-${String.format("%02d",cal[Calendar.MONTH]+1)}"
+        val startDate = "$yearMonth-${String.format("%02d",cal.getActualMinimum(Calendar.DAY_OF_MONTH))}"
+        val endDate = "$yearMonth-${String.format("%02d",cal.getActualMaximum(Calendar.DAY_OF_MONTH))}"
         setPeriod(startDate, endDate)
+    }
+
+    private fun <T> gsonToJson(list: List<T>): String? {
+        return Gson().toJson(list)
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/org/passorder/boss/presentation/main/history/HistoryViewModel.kt
@@ -0,0 +1,71 @@
+package org.passorder.boss.presentation.main.history
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.passorder.domain.entity.Order
+import org.passorder.domain.entity.SetCount
+import org.passorder.domain.entity.SetOrder
+import org.passorder.domain.repository.OrderRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryViewModel @Inject constructor(
+    private val repository: OrderRepository
+) : ViewModel() {
+    private val _currentOrder = MutableStateFlow<List<Order>>(listOf())
+    val currentOrder = _currentOrder.asStateFlow()
+
+    private val _money = MutableStateFlow(0)
+    val money = _money.asStateFlow()
+
+    private val _orderCount = MutableStateFlow(0)
+    val orderCount = _orderCount.asStateFlow()
+
+
+
+    fun orderList(request: SetOrder) {
+        viewModelScope.launch {
+            runCatching {
+                repository.orderList(request)
+            }.onSuccess {
+                _currentOrder.value = it
+                //todo 리스트 동적으로 추가
+            }.onFailure {
+                Log.e("ERROR", it.toString())
+            }
+        }
+    }
+
+    fun getOrderCount(request: SetCount) {
+        viewModelScope.launch {
+            runCatching {
+                repository.orderCount(request)
+            }.onSuccess {
+                var count = 0
+                it.map { response ->
+                    count += response.count
+                }
+                _orderCount.value = count
+            }
+        }
+    }
+
+    fun getTotalMoney(request: SetCount) {
+        viewModelScope.launch {
+            runCatching {
+                repository.totalMoney(request)
+            }.onSuccess {
+                var money = 0
+                it.map { response ->
+                    money += response.usedPassorderPoint
+                }
+                _money.value = money
+            }
+        }
+    }
+}
