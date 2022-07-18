265f7026684073ac061ad671748d4cd46eb32b23
kkangminlee <lkm@paytalab.com>
2022-07-18 17:49:12 +0900

    [#15] 공지사항 BindingAdapter Setting

--- app/src/main/java/org/passorder/boss/util/BindingAdapter.kt
+++ app/src/main/java/org/passorder/boss/util/BindingAdapter.kt
@@ -3,6 +3,7 @@ package org.passorder.boss.util
 import android.annotation.SuppressLint
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import org.passorder.boss.R
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,7 +11,7 @@ object BindingAdapter {
     @SuppressLint("SetTextI18n")
     @JvmStatic
     @BindingAdapter("text_price")
-    fun setText(view: TextView, price: Int) {
+    fun setPriceText(view: TextView, price: Int) {
         view.text = "금액 : ${price}원"
     }
 
@@ -20,5 +21,17 @@ object BindingAdapter {
         val pattern = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH).parse(date)
         val koreaDate = SimpleDateFormat("yyyy년 MM월 dd일 E요일 a hh시 mm분", Locale.KOREA).format(pattern)
         view.text = koreaDate
+    }
+
+    @JvmStatic
+    @BindingAdapter("text_notice_menu")
+    fun setNoticeMenuText(view: TextView, email: String, menu: String) {
+        view.text = "$email 패써님이 $menu 메뉴를 원해요 !"
+    }
+
+    @JvmStatic
+    @BindingAdapter("text_notice_store")
+    fun setNoticeStoreText(view: TextView, email: String) {
+        view.text = "$email 패써님이 매장 오픈을 원하고 있어요"
     }
 }
