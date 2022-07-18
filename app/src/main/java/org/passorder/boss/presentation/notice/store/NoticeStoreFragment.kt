b29d94d3d5a996e815bf775814c71ad4c5c06950
kkangminlee <lkm@paytalab.com>
2022-07-18 16:15:52 +0900

    [#15] NoticeMenuFragment & Store

---
+++ app/src/main/java/org/passorder/boss/presentation/notice/store/NoticeStoreFragment.kt
@@ -0,0 +1,15 @@
+package org.passorder.boss.presentation.notice.store
+
+import android.os.Bundle
+import android.view.View
+import org.passorder.boss.R
+import org.passorder.boss.databinding.FragmentNoticeStoreBinding
+import org.passorder.ui.base.BindingFragment
+
+class NoticeStoreFragment: BindingFragment<FragmentNoticeStoreBinding>(R.layout.fragment_notice_store) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
