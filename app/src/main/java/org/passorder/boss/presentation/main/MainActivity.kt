f6c904915f8549283255169c461f122786b98943
kkangminlee <lkm@paytalab.com>
2022-07-20 11:11:38 +0900

    [#17] 매장 오픈 값 적용

--- app/src/main/java/org/passorder/boss/presentation/main/MainActivity.kt
+++ app/src/main/java/org/passorder/boss/presentation/main/MainActivity.kt
@@ -2,6 +2,7 @@ package org.passorder.boss.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -25,7 +26,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     @Inject
     lateinit var dataStore: PassDataStore
     private val viewModel by viewModels<MainViewModel>()
-    val time = listOf(0,5,10,15,20,30,40,60)
+    val time = listOf(0, 5, 10, 15, 20, 30, 40, 60)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +39,14 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     private fun initView() {
         binding.viewModel = viewModel
         binding.spinnerTime.apply {
-            adapter = ArrayAdapter.createFromResource(this@MainActivity, R.array.spinner_List, android.R.layout.simple_spinner_dropdown_item)
+            adapter = ArrayAdapter.createFromResource(
+                this@MainActivity,
+                R.array.spinner_List,
+                android.R.layout.simple_spinner_dropdown_item
+            )
 
-            binding.vpMenu.adapter = MainViewPagerAdapter(supportFragmentManager.fragmentFactory, this@MainActivity)
+            binding.vpMenu.adapter =
+                MainViewPagerAdapter(supportFragmentManager.fragmentFactory, this@MainActivity)
 
             TabLayoutMediator(binding.tlMenu, binding.vpMenu) { tab, position ->
                 tab.text = stringListFrom(R.array.tab_list)[position]
@@ -50,17 +56,22 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     private fun initEvent() {
         var isFirstSelected = true
+
+        // 매장 열고 닫기 서버 통신 이벤트
         binding.ivCheckOpen.setOnClickListener {
             viewModel.gateStore(!binding.ivCheckOpen.isSelected)
+            Log.d("kangmi", (!binding.ivCheckOpen.isSelected).toString())
         }
 
+        // 공지사항 뷰로 이동하는 이벤트
         binding.ivAlarm.setOnClickListener {
             Intent(this, NoticeActivity::class.java).apply {
                 startActivity(this)
             }
         }
 
-        binding.spinnerTime.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        // 메뉴 최소 수령시간 설정 이벤트
+        binding.spinnerTime.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
                 if (isFirstSelected) {
                     isFirstSelected = false
@@ -82,8 +93,11 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
         viewModel.storeInfo.flowWithLifecycle(lifecycle)
             .onEach {
-                binding.tvName.text = it.name
-                binding.spinnerTime.setSelection(time.indexOf(it.minimumPickupTime))
+                binding.apply {
+                    tvName.text = it.name
+                    spinnerTime.setSelection(time.indexOf(it.minimumPickupTime))
+                    ivCheckOpen.isSelected = it.isOpen
+                }
             }.launchIn(lifecycleScope)
     }
 }
