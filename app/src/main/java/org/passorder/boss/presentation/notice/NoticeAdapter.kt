4839b2a6cdfa3c2702cdd3bbeb2ca71a066ea6f6
kkangminlee <lkm@paytalab.com>
2022-07-20 17:20:58 +0900

    [#19] NoticeAdapter

---
+++ app/src/main/java/org/passorder/boss/presentation/notice/NoticeAdapter.kt
@@ -0,0 +1,38 @@
+package org.passorder.boss.presentation.notice
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.passorder.boss.databinding.ItemNoticeCommonBinding
+import org.passorder.domain.entity.Notice
+
+// 공지사항(매장 미오픈, 메뉴 품절)을 위한 리사이클러뷰 어뎁터
+class NoticeAdapter : RecyclerView.Adapter<NoticeAdapter.NoticeStoreViewHolder>() {
+    private val data = mutableListOf<Notice>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeStoreViewHolder {
+        val binding =
+            ItemNoticeCommonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoticeStoreViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoticeStoreViewHolder, position: Int) {
+        holder.onBind(data[position])
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    fun setItems(newItems: List<Notice>) {
+        data.clear()
+        data.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    class NoticeStoreViewHolder(
+        private val binding: ItemNoticeCommonBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(notice: Notice) {
+            binding.data = notice
+        }
+
+    }
+}
