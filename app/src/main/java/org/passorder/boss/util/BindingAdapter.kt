e4a41279c4b7c7ec110f137ae8136b1524e097dc
kkangminlee <lkm@paytalab.com>
2022-07-20 16:42:59 +0900

    [#19] DateFormat 설정

--- app/src/main/java/org/passorder/boss/util/BindingAdapter.kt
+++ app/src/main/java/org/passorder/boss/util/BindingAdapter.kt
@@ -24,14 +24,10 @@ object BindingAdapter {
     }
 
     @JvmStatic
-    @BindingAdapter("text_notice_menu")
-    fun setNoticeMenuText(view: TextView, email: String, menu: String) {
-        view.text = "$email 패써님이 $menu 메뉴를 원해요 !"
-    }
-
-    @JvmStatic
-    @BindingAdapter("text_notice_store")
-    fun setNoticeStoreText(view: TextView, email: String) {
-        view.text = "$email 패써님이 매장 오픈을 원하고 있어요"
+    @BindingAdapter("date_format_notice")
+    fun dataFormatNotice(view: TextView, date: String) {
+        val pattern = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH).parse(date)
+        val koreaDate = SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분", Locale.KOREA).format(pattern)
+        view.text = koreaDate
     }
 }
