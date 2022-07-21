6f2e20b55f6b045cb0e3fd5b1462dfe7beb35733
kkangminlee <lkm@paytalab.com>
2022-07-21 17:48:56 +0900

    [#21] OrderPagingSource 주석 추가

--- data/src/main/java/org/passorder/data/datasource/remote/OrderPagingSource.kt
+++ data/src/main/java/org/passorder/data/datasource/remote/OrderPagingSource.kt
@@ -10,6 +10,7 @@ class OrderPagingSource(
     private val service: OrderService,
     private val request: RequestOrder
 ) : PagingSource<Int, ResponseOrder>() {
+    // 현재 페이지 포지션을 가지고 있는 로직
     override fun getRefreshKey(state: PagingState<Int, ResponseOrder>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
@@ -17,6 +18,7 @@ class OrderPagingSource(
         }
     }
 
+    // 스크롤을 할 시 현재 페이지를 계속해서 증가하며 서버통신을 할 수 있는 로직, 위로 스크롤 할 시 현재페이즈를 감소시키며 서버통신
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ResponseOrder> {
         val currentPosition = params.key ?: 1
         val response = runCatching {
@@ -28,7 +30,7 @@ class OrderPagingSource(
             )
         }.getOrElse { return LoadResult.Error(it) }
 
-        val nextPosition = if( response.isEmpty()) null else currentPosition + 1
+        val nextPosition = if(response.isEmpty()) null else currentPosition + 1
         val previousPosition =
             if (currentPosition != 0) null else currentPosition - 1
         return runCatching {
